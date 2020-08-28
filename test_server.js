62648e81754b4c73d306590c54b6a08057b74828
shadedstrike <pyrobonsly@gmail.com>
2020-08-28 15:40:52 -0600

    adding all the screenshots

--- test_server.js
+++ test_server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello TA! I've never typed in javascript before, how exciting\n');
 });
 
 server.listen(port, hostname, () => {
