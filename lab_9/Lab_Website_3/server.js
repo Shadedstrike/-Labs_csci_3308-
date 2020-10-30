059acafb56f08a4e4d27eb40b0af6f78b51ca5a3
shadedstrike <apot7080@colorado.edu>
2020-10-30 12:17:00 -0600

    update webside and js

--- lab_9/Lab_Website_3/server.js
+++ lab_9/Lab_Website_3/server.js
@@ -68,8 +68,8 @@ app.use(express.static(__dirname + '/'));//This line is necessary for us to use 
 
   		/team_stats - get request (no parameters)
   			This route will require no parameters.  It will require 3 postgres queries which will:
-  				1. Retrieve all of the football games in the Fall 2018 Season
-  				2. Count the number of winning games in the Fall 2018 Season
+  				1. Retrieve all of the wifootball games in the Fall 2018 Season
+  				2. Count the number of nning games in the Fall 2018 Season
   				3. Count the number of lossing games in the Fall 2018 Season
   			The three query results will then be passed onto the team_stats view (pages/team_stats).
   			The team_stats view will display all fo the football games for the season, show who won each game,
@@ -120,25 +120,36 @@ app.get('/home', function(req, res) {
 });
 
 
-app.post('/home/pick_color', function(req, res) {
-	var color_hex = req.body.color_hex;
-	var color_name = req.body.color_name;
-	var color_message = req.body.color_message;
-	var insert_statement =  'select * from favorite_colors;';// Write a SQL statement to insert a color into the favorite_colors table
-	var color_select =  'select * from favorite_colors;';// Write a SQL statement to retrieve all of the colors in the favorite_colors table
+app.get('/home/pick_color', function(req, res) {
 
+  console.warn("req,query = ", req.query);
+
+
+	var color_choice = req.query.color_selection; // Investigate why the parameter is named "color_selection"
+  console.warn("color choice is = ", color_choice);
+
+	var color_options = 'select * from favorite_colors;' // Write a SQL query to retrieve the colors from the database
+	var color_message = 'SELECT * FROM favorite_colors WHERE hex_value = ${color_selection};';// Write a SQL query to retrieve the color message for the selected color
 	db.task('get-everything', task => {
         return task.batch([
-            task.any(insert_statement),
-            task.any(color_select)
+            task.any(color_options, req.query),
+            task.any(color_message, req.query)
         ]);
     })
     .then(info => {
+      console.warn("app get pick_color", info);
+
+      const colors = info[0]
+      const chosen_color = info[1][0]
+
+      console.warn("chosen_color =", chosen_color);
+
+
     	res.render('pages/home',{
 				my_title: "Home Page",
-				data: // Return the color choices
-				color: // Return the hex value of the color added to the table
-				color_msg: // Return the color message of the color added to the table
+				data: colors , // Return the color options
+				color:chosen_color.hex_value, // Return the color choice
+				color_msg: chosen_color.color_msg // Return the color message
 			})
     })
     .catch(err => {
@@ -150,7 +161,194 @@ app.post('/home/pick_color', function(req, res) {
                 color_msg: ''
             })
     });
+
+});////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/team_stats', function(req, res) {
+
+	var fall_games = `SELECT * FROM football_games WHERE game_date >= '2020-08-01'::DATE AND game_date <='2021-02-07'::DATE ORDER BY game_date;`;
+	var fall_wins = `SELECT COUNT(*) FROM football_games WHERE home_score > visitor_score AND game_date >= '2020-08-01'::DATE AND game_date <='2021-02-07'::DATE;`;
+	var fall_losses = `SELECT COUNT(*) FROM football_games WHERE home_score < visitor_score AND game_date >= '2020-08-01'::DATE AND game_date <='2021-02-07'::DATE;`;
+	db.task('get-football-games', task => {
+        return task.batch([
+            task.any(fall_games, req.query),
+            task.any(fall_wins, req.query),
+            task.any(fall_losses, req.query)
+        ]);
+    })
+    .then(info => {
+      const [all, wins, losses] = info;
+      // const all = info[0];
+      // const wins = info[1];
+      // const losses = info[2];
+
+    	res.render('pages/team_stats',{
+				my_title: "Team Stats",
+				all,
+				wins,
+				losses,
+
+			})
+    })
+    .catch(err => {
+            console.log('error', err);
+            res.render('pages/team_stats', {
+                my_title: 'Team stats',
+                all: '',
+                wins: [{"count":'error'}],
+                losses: [{"count":'error'}],
+            })
+    });
+
 });
+
+
+
+app.get('/player_info', function(req, res) {
+
+	var select_players = `SELECT id,name FROM football_players;`;
+	db.task('get-football-players', task => {
+    return task.batch([
+      task.any(select_players , req.query),
+    ]);
+  })
+  .then(info => {
+    const [players] = info;
+    console.log(players, info);
+    res.render('pages/player_info',{
+      my_title: "player info",
+      player: false,
+      players
+    })
+  }).catch(err => {
+    console.log('error', err);
+    res.render('pages/player_info', {
+      my_title: 'Player info',
+      player: false,
+      players: [{'id':0,'name':'error'}],
+      
+    })
+  });
+  
+});
+
+
+
+app.get('/player_info/post', function(req, res) {
+  console.log(req.query.player_choice);
+  let id = req.query.player_choice;
+  var select_players = `SELECT id,name FROM football_players;`;
+  if(Number.isNaN(Number(id))){
+    id = 0 
+  }
+  var select_player = `SELECT * FROM football_players WHERE id = ${Number(id)} LIMIT 1`;
+  var select_games = `SELECT COUNT(*) FROM football_games WHERE ${Number(id)} = ANY (players);`;
+  var select_averages = `SELECT AVG(passing_yards) AS passing, AVG(rushing_yards) AS rushing, AVG(receiving_yards) AS receiving FROM football_players;`;
+	db.task('get-football-players', task => {
+    return task.batch([
+      task.any(select_players , req.query),
+      task.any(select_player , req.query),
+      task.any(select_games , req.query),
+      task.any(select_averages , req.query),
+    ]);
+  })
+  .then(info => {
+      var [players, player, games, averages] = info;
+      console.log(player, info);
+      if(player.length === 0) player = false;
+      else player = player[0]
+    	res.render('pages/player_info',{
+        my_title: "player info",
+        players,
+        player,
+        games: games[0].count, 
+        averages: averages[0]
+			})
+    }).catch(err => {
+            console.log('error', err);
+            res.render('pages/player_info', {
+                my_title: 'Player info',
+                players: [{'id':0,'name':'error'}],
+                player: false
+            })
+    });
+});
+
+
+
+//await db.none('INSERT INTO users(first_name, last_name, age) VALUES(${name.first}, $<name.last>, $/age/)', {
+    //name: {first: 'John', last: 'Dow'},
+  //  age: 30
+//}/);
+
+
+
+app.post('/home/pick_color', function(req, res) {
+  console.warn("req, body = req,body", req.body);
+  console.warn("pasta");
+
+  try {
+    var color_hex = req.body.color_hex;
+    var color_name = req.body.color_name;
+    var color_message = req.body.color_message;
+    console.warn("VALUES (${color_hex}, ${color_name}, ${color_message =", color_hex, color_name, color_message);
+
+    var insert_statement =  'INSERT INTO favorite_colors(hex_value, name, color_msg) VALUES (${color_hex}, ${color_name}, ${color_message}) RETURNING *;';// Write a SQL statement to insert a color into the favorite_colors table
+    var color_select =  'select * from favorite_colors;';// Write a SQL statement to retrieve all of the colors in the favorite_colors table
+    const color_to_add = {
+      color_hex,
+      color_name,
+      color_message,
+    }
+
+    db.task('get-everything', task => {
+          return task.batch([
+              task.any(insert_statement, color_to_add),
+              task.any(color_select)
+          ]);
+      })
+      .then(info => {
+        console.warn("colors LALALALLAL =",info);
+
+        const newcolor = info[0]
+        console.warn("newcolor =",  newcolor)
+
+        res.render('pages/home',{
+          my_title: "Home Page",
+          data: info[1],// Return the color choices
+          color: newcolor.hex_value, // Return the hex value of the color added to the table
+          color_msg: newcolor.color_msg,// Return the color message of the color added to the table
+        })
+      })
+      .catch(err => {
+              console.log('error', err);
+              console.log('pago');
+
+              res.render('pages/home', {
+                  my_title: '404 not found fam',
+                  data: '',
+                  color: '',
+                  color_msg: ''
+              })
+      });
+
+
+  } catch (e) {
+    console.log('error: ', e);
+
+    res.render('pages/home', {
+        my_title: 'request body not found',
+        data: '',
+        color: '',
+        color_msg: ''
+    })
+
+  }
+});
+
+
+
+
 
 app.listen(3000);
 console.log('3000 is the magic port');
